d211f31304e24f288d417f445c3ca883380ea7c6
sigdelbjay <sigdelbjay@gmail.com>
2020-12-17 23:26:52 +1100

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/CarComponent.java
@@ -0,0 +1,8 @@
+package com.example.myapplication;
+
+import dagger.Component;
+
+@Component
+public interface CarComponent {
+    Car getCar();
+}
---
+++ app/src/main/java/com/example/myapplication/Wheels.java
@@ -0,0 +1,11 @@
+package com.example.myapplication;
+
+import javax.inject.Inject;
+
+public class Wheels {
+
+    @Inject
+    public Wheels() {
+
+    }
+}
