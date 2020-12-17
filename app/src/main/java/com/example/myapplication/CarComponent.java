d4e893f5f747153734721f9a2a6294613c067779
sigdelbjay <sigdelbjay@gmail.com>
2020-12-18 08:44:44 +1100

    Field injection

--- app/src/main/java/com/example/myapplication/CarComponent.java
+++ app/src/main/java/com/example/myapplication/CarComponent.java
@@ -4,5 +4,7 @@ import dagger.Component;
 
 @Component
 public interface CarComponent {
-    Car getCar();
+//    Car getCar();
+
+    void inject(MainActivity mainActivity);
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,16 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import javax.inject.Inject;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Car car;
+//    private Car car;
+    @Inject Car car;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         CarComponent component = DaggerCarComponent.create();
-        car = component.getCar();
+//        car = component.getCar();
+        component.inject(this);
         car.drive();
     }
 }
